317c09f52a45bb49fe92eca358b313e6b67fd17a
Fabrizio Cafolla <fabrizio.cafolla97@gmail.com>
2018-11-03 18:26:46 +0100

    init commit

---
+++ src/core/Services/Api/ApiService.php
@@ -0,0 +1,17 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: fabrizio
+	 * Date: 31/07/18
+	 * Time: 16.30
+	 */
+
+	namespace App\Services;
+
+	use App\Helpers\Traits\Fractal;
+
+
+	class ApiService
+	{
+		use Fractal;
+	}
